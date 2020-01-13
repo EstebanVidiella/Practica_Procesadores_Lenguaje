1aedaa7aba734974713bdcebee6611982008614d
bruno99 <43136694+bruno99@users.noreply.github.com>
2020-01-13 17:00:42 +0100

    Create TipoPrimitivo.java

---
+++ practica_4/TipoPrimitivo.java
@@ -0,0 +1,12 @@
+package Practica_4;
+
+public class TipoPrimitivo extends TipoDato{
+	
+	public TipoPrimitivo(String tipo) {
+		super(tipo);
+	}
+	
+	public String toString() {
+		return super.getTipo();
+	}
+}
