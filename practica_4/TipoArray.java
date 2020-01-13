4f75aac55e6739ac4db5c8e0c0c0efac88d2df83
bruno99 <43136694+bruno99@users.noreply.github.com>
2020-01-13 17:01:30 +0100

    Create TipoArray.java

---
+++ practica_4/TipoArray.java
@@ -0,0 +1,22 @@
+package Practica_4;
+
+public class TipoArray extends TipoDato{
+	private int tamaño;
+	
+	public TipoArray(String tipo, int tamaño) {
+		super(tipo);
+		this.tamaño = tamaño;
+	}
+	
+	public String toString() {
+		return super.getTipo() + "[" + this.tamaño + "]";
+	}
+
+	public int getTamaño() {
+		return tamaño;
+	}
+
+	public void setTamaño(int tamaño) {
+		this.tamaño = tamaño;
+	}
+}
